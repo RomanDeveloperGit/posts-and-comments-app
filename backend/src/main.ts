e4c7968f25ef905f30e42ea4729a2c2398226e38
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-07-09 14:48:16 +0300

    fix(backend): fix env and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe());
